e56ec74661ff31730cddfb763207998990bfbd91
ChocolateCupcake25 <92889263+ChocolateCupcake25@users.noreply.github.com>
2022-06-26 15:00:42 +0100

    Add files via upload

---
+++ IMAGE_VIEWER.py.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 09:12:30 2022
+
+@author: HP
+"""
+
+from tkinter import*
+from PIL import ImageTk,Image
+from tkinter import filedialog
+
+root= Tk()
+root.title(" IMAGE_VIEWER")
+root.geometry("500x500")
+root.configure(bg="medium slate blue")
+
+lable_image=Label(root, bg="misty rose",highlightthickness=5,borderwidth=2,relief=SOLID)
+lable_image.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+img_path=""
+def OpenImage():
+    global img_path
+    img_path=filedialog.askopenfilename(title="Open Image Files", filetypes= [("Image Files", "*.jpg; *.gif; *.png; *.jpeg;*txt")])
+    print(img_path)
+    img=ImageTk.PhotoImage(Image.open(img_path))
+    lable_image["image"]=img
+    img.close()
+
+openImage=Button(root,text="Open Image", command=OpenImage)
+openImage.place(relx=0.5,rely=0.1,anchor=CENTER)
+
+
+
+root.mainloop()
