971d8425ea13da2a858ea4bd5057ffe3cc625866
ChocolateCupcake25 <92889263+ChocolateCupcake25@users.noreply.github.com>
2022-06-26 17:42:47 +0100

    Add files via upload

--- IMAGE_VIEWER.py.py
+++ IMAGE_VIEWER.py.py
@@ -25,10 +25,23 @@ def OpenImage():
     img=ImageTk.PhotoImage(Image.open(img_path))
     lable_image["image"]=img
     img.close()
+    
+def RotateImage():
+    global img_path
+    im = Image.open(img_path)
+    rotated_img=im.rotate(180)
+    img=ImageTk.PhotoImage(Image.open(rotated_img))
+    lable_image["image"]=img
+    img.close()
+    
 
-openImage=Button(root,text="Open Image", command=OpenImage)
+openImage=Button(root,text="Open Image", command=OpenImage,bg="lightSalmon2",font=("castellar",10,"bold"),relief=RIDGE,borderwidth=5)
 openImage.place(relx=0.5,rely=0.1,anchor=CENTER)
 
+rotateImage=Button(root,text="Rotate Image",command=RotateImage,bg="lightSalmon2",font=("castellar",10,"bold"),relief=RIDGE,borderwidth=5)
+rotateImage.place(relx=0.5,rely=0.8,anchor=CENTER)
 
+label=Label(root,text="Created by:Ziyah ",bg="medium slate blue")
+label.place(relx=0.5,rely=0.95,anchor=CENTER)
 
 root.mainloop()
